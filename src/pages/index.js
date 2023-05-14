8a29a0583df027a75ab9fb3f736fb04b68c60477
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-14 10:53:32 -0300

    Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
-
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../styles/indexStyle'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+   <Container>
+     <Content>
+      <Infos>
+        <Name>Olá, sou Gabriel Estéfono</Name>
+        <Function>FullStack Developer</Function>
+        <Intro>Estudante de Engenharia de Software.
+        </Intro>
+        <Link href="projects">
+          <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+        </Link>
+      </Infos>
+      <Logo>
+        <Img src="/images/foto.png" alt="logo" />
+      </Logo>
+     </Content>
+   </Container>
   )
 }
